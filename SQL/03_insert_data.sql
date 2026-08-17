7c687f92ebdfe8972757fae1aa0370c03e30aab8
Mahtab <154454735+Mooooony20@users.noreply.github.com>
2026-08-17 18:54:01 -0400

    Add files via upload

---
+++ SQL/03_insert_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO ecommerce.Customers
+(FirstName, LastName, Email, City, Country)
+
+VALUES
+('John','Smith','john@email.com','Toronto','Canada'),
+('Sarah','Brown','sarah@email.com','Montreal','Canada'),
+('Ali','Ahmadi','ali@email.com','Tehran','Iran'),
+('Emma','Wilson','emma@email.com','Vancouver','Canada'),
+('David','Miller','david@email.com','Calgary','Canada');
+
+
+
+INSERT INTO ecommerce.Products
+(ProductName, Category, Price)
+
+VALUES
+('Laptop','Electronics',1200),
+('Mouse','Electronics',30),
+('Keyboard','Electronics',70),
+('Office Chair','Furniture',250),
+('Desk','Furniture',400);
